ecfafae49c3e84e00e2da56f2ea090d56709f4c3
KarimAldeen <MhmadKarimAldeen@gmail.com>
2023-07-27 00:37:57 +0300

    asdf

--- src/app/services/FirstService/page.js
+++ src/app/services/FirstService/page.js
@@ -4,58 +4,95 @@ import NavBar from '@/Components/NavBar'
 import React from 'react'
 import Image from './Image'
 
-function FirstService() {  
-    const Transtion = () => {
-        document.getElementById("Transion_page").classList.add("Transion_page")
-        // setTimeout(() => {
+function FirstService() {
+  const Transtion_Page2 = () => {
+    document.getElementById("Transion_page").classList.remove("Back_page")
+    document.getElementById("Transion_page").classList.add("Transion_page")
+    setTimeout(() => {
 
-        //   document.getElementById("Wing").classList.add("None")
-        //   document.getElementById("Hajeat_Moaz").src = "../Services2/Hajeat_Wing.png"
-        //   document.getElementById("Hajeat_Moaz").classList.add("Hajeat_Wing")
+      document.getElementById("Transion_page").classList.add("Timer")
+      
 
-        // }, "500");
+    }, "500");
+
+  }
+  const Transtion_Page3 = () => {
+    document.getElementById("Transion_page").classList.add("Transion_page3")
+
+  }
+  const Back_Page1 = () => {
+    document.getElementById("Transion_page").classList.remove("Transion_page")
+    document.getElementById("Transion_page").classList.add("Back_page")
+
 
   }
   return (
     <div className='Services_Pages' id='Transion_page'>
-            <NavBar/>
-    <div className='FirstService_page'>
-    <div className='FirstService_Images'>
-    {/* <img src='../Services1/Hajeat_Moaz.png' alt='' className='Hajeat_Moaz' id='Hajeat_Moaz'/> */}
+      <NavBar />
+      <div className='FirstService_page'>
+        <div className='FirstService_Images'>
 
-    <Image/>
+          <Image />
 
-    <img src='../Services1/Haje.png' alt='' className='Haje'/>
+          <img src='../Services1/Haje.png' alt='' className='Haje' />
 
-    </div>
-    <div className='FirstService_Body'>
-      <div className='Services1_Elements'>
+        </div>
+        <div className='FirstService_Body'>
+          <div className='Services1_Elements'>
 
-        <img src='../Services_Elements/1_Section1.svg' alt='' className='Element1'/>
-      <div onClick={()=>Transtion()}>
-      <img  src='../Services_Elements/2_Section1.svg' alt='' className='Element2'/>
+            <img src='../Services_Elements/1_Section1.svg' alt='' className='Element1' />
+            <div onClick={() => Transtion_Page2()}>
+              <img src='../Services_Elements/1_Section2.svg' alt='' className='Element2' />
+            </div>
+
+          </div>
+        </div>
       </div>
 
-      </div>
-      </div>
-    </div>
-  
-    <div className='SecondService_page'>
-    <div className='SecondService_Images'>
-    {/* <img src='../Services1/Haje.png' alt='' className='Haje'/> */}
+      <div className='SecondService_page'>
+        <div className='SecondService_Images'>
+          <img src='../Services2/Girl_Logo.png' alt='' className='Girl_Logo' />
+          <img src='../Services2/Polygon_Right.png' alt='' className='Polygon_Right ' />
 
-    </div>
-    <div className='SecondService_Body'>
-      {/* <div className='Services1_Elements'>
+        </div>
+        <div className='SecondService_Body'>
+          <div className='Services2_Elements'>
+            <div className='Elements_2_1'>
+              <img src='../Services_Elements/2_Section2.svg' alt='' className='Element2' />
+              <img src='../Services_Elements/2_Section1.svg'  alt='' className='Element1' onClick={() => Back_Page1()} />
+            </div>
 
-        <img src='../Services_Elements/1_Section1.svg' alt='' className='Element1'/>
-      <div onClick={()=>Transtion()}>
-      <img  src='../Services_Elements/2_Section1.svg' alt='' className='Element2'/>
+
+            <div >
+              <img src='../Services_Elements/2_Section3.svg' alt='' className='Element3'onClick={() => Transtion_Page3()}  />
+
+            </div>
+
+          </div>
+        </div>
       </div>
+      {/* <div className='ThirdService_page'>
+        <div className='ThirdService_Images'>
+          <img src='../Services2/Girl_Logo.png' alt='' className='Girl_Logo' />
+          <img src='../Services2/Polygon_Right.png' alt='' className='Polygon_Right ' />
 
+        </div>
+        <div className='ThirdService_Body'>
+          <div className='Services2_Elements'>
+            <div className='Elements_2_1'>
+              <img src='../Services_Elements/2_Section2.svg' alt='' className='Element2' />
+              <img src='../Services_Elements/2_Section1.svg'  alt='' className='Element1' onClick={() => Back_Page1()} />
+            </div>
+
+
+            <div >
+              <img src='../Services_Elements/2_Section3.svg' alt='' className='Element3' />
+
+            </div>
+
+          </div>
+        </div>
       </div> */}
-      </div>
-    </div>
     </div>
   )
 }
