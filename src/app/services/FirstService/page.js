57a83ad947888c828cc9b429ca92499da04ceb72
KarimAldeen <MhmadKarimAldeen@gmail.com>
2023-07-26 15:06:00 +0300

    asdasd

--- src/app/services/FirstService/page.js
+++ src/app/services/FirstService/page.js
@@ -1,26 +1,59 @@
+"use client"
+
 import NavBar from '@/Components/NavBar'
 import React from 'react'
-import { FaFacebookF, FaTiktok, FaWhatsapp, FaSnapchatGhost, FaInstagram, FaLinkedinIn } from "react-icons/fa";
-import FirstServiceBG from '../../../Components/Services/FirstServiceBG'
 
-function FirstService() {
+function FirstService() {  
+    const Transtion = () => {
+        document.getElementById("Transion_page").classList.add("Transion_page")
+        // setTimeout(() => {
+
+        //   document.getElementById("Wing").classList.add("None")
+        //   document.getElementById("Hajeat_Moaz").src = "../Services2/Hajeat_Wing.png"
+        //   document.getElementById("Hajeat_Moaz").classList.add("Hajeat_Wing")
+
+        // }, "500");
+
+  }
   return (
-    <>
+    <div className='Services_Pages' id='Transion_page'>
+            <NavBar/>
     <div className='FirstService_page'>
-      <FirstServiceBG/>
-      <NavBar/>
-      <img className='FirstService_page' src='../Services/FirstService/FirstService_BG.' alt=''/>
+    <div className='FirstService_Images'>
+    <img src='../Services1/Hajeat_Moaz.png' alt='' className='Hajeat_Moaz' id='Hajeat_Moaz'/>
+    <img src='../Services1/Haje.png' alt='' className='Haje'/>
 
-      <div className='FirstService_icons'>
-          <FaFacebookF/>
-          <FaTiktok/>
-          <FaInstagram/>
-          <FaWhatsapp/>
-          <FaLinkedinIn/>
-          <FaSnapchatGhost/>
-        </div>
     </div>
-    </>
+    <div className='FirstService_Body'>
+      <div className='Services1_Elements'>
+
+        <img src='../Services_Elements/1_Section1.svg' alt='' className='Element1'/>
+      <div onClick={()=>Transtion()}>
+      <img  src='../Services_Elements/2_Section1.svg' alt='' className='Element2'/>
+      </div>
+
+      </div>
+      </div>
+    </div>
+  
+    <div className='SecondService_page'>
+    <div className='SecondService_Images'>
+    <img src='../Services2/Wing.png' alt='' className='Wing' id='Wing'/>
+    {/* <img src='../Services1/Haje.png' alt='' className='Haje'/> */}
+
+    </div>
+    <div className='SecondService_Body'>
+      {/* <div className='Services1_Elements'>
+
+        <img src='../Services_Elements/1_Section1.svg' alt='' className='Element1'/>
+      <div onClick={()=>Transtion()}>
+      <img  src='../Services_Elements/2_Section1.svg' alt='' className='Element2'/>
+      </div>
+
+      </div> */}
+      </div>
+    </div>
+    </div>
   )
 }
 
