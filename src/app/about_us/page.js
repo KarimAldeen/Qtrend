93f3fe46f81cd4bcb2c40deb61525d522e35ac63
ibrahimkarimeddin <ibrahimkarimeddin@gmail.com>
2023-07-27 20:14:03 +0300

    PUSH

--- src/app/about_us/page.js
+++ src/app/about_us/page.js
@@ -15,11 +15,12 @@ function AboutUs() {
         <div className='AboutUs_mid_section'>
 
         <p className='AboutUs_p'> 
-            QTREND is a leading digital marketing agency that specializes in providing innovative and result-driven <br/> solutions to help businesses thrive in the digital landscape. With a team of seasoned experts and a deep<br/> understanding of the latest trends in online marketing, we empower our clients to achieve their marketing <br/>goals and stay ahead of the competition.<br/>
-            Our Approach:<br/>
-            At QTREND, we believe in the power of creativity, data-driven strategies, and cutting-edge technology to<br/> deliver exceptional results. We tailor our approach to each client s unique needs and develop customized<br/> digital marketing campaigns that drive measurable outcomes.<br/>
-            Services:<br/>
-            We offer a comprehensive range of digital marketing services that encompass the entire spectrum of online <br/> promotion. Our services include search engine optimization (SEO), pay-per-click (PPC) advertising, social <br/> media marketing, content creation, email marketing, conversion rate optimization, and more. By leveraging <br/> multiple channels, we ensure maximum visibility and engagement for our clients brands.
+            QTREND is a leading digital marketing agency that specializes in providing innovative and result-driven  solutions to help businesses thrive in the digital landscape. With a team of seasoned experts and a deep understanding of the latest trends in online marketing, we empower our clients to achieve their marketing goals and stay ahead of the competition.
+            Our Approach:
+            At QTREND, we believe in the power of creativity, data-driven strategies, and cutting-edge technology to deliver exceptional results. We tailor our approach to each client s unique needs and develop customized  digital marketing campaigns that drive measurable outcomes.
+            Services:
+            We offer a comprehensive range of digital marketing services that encompass the entire spectrum of online  promotion. Our services include search engine optimization (SEO), pay-per-click (PPC) advertising, social  media marketing, content creation, email marketing, conversion rate optimization, and more. By leveraging  multiple channels, we ensure maximum visibility and engagement for our clients brands.
+          
         </p> 
         <div className='AboutUs_icons'>
           <FaFacebookF/>
@@ -31,7 +32,9 @@ function AboutUs() {
         </div>
         <div className='AboutUs_info'>
           <h1>Partner with QTREND:</h1>
-          <h6>If you are seeking a trusted digital marketing partner to elevate your online presence and drive business growth, look no further than <br/> Qtrend. With our unwavering focus on innovation, data-driven strategies, and client success, we are here to help you unlock the full <br/> potential of digital marketing. Contact us today to embark on an exciting journey toward digital success.</h6>
+          <h6>If you are seeking a trusted digital marketing partner to elevate your online <br/>
+          presence and drive business growth, look no further than  Qtrend. With our unwavering focus on innovation, data-driven strategies, and client success, we are here to help you unlock the full  potential of digital marketing. Contact us today to embark on an exciting journey toward digital success.
+          </h6>
         </div>
         <Footer/>
 
