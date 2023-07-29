1d691a7630fda52dbb794368accff73f52efa7ef
KarimAldeen <MhmadKarimAldeen@gmail.com>
2023-07-29 17:31:16 +0300

    asfa

---
+++ src/app/test/Test.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <svg
+  xmlns="http://www.w3.org/2000/svg"
+  xmlnsXlink="http://www.w3.org/1999/xlink"
+  width={890}
+  height={736}
+  viewBox="0 0 890 736"
+  className='Svg'
+>
+  <g id="Group_3" data-name="Group 3" transform="translate(-218 -59)">
+    <image
+    className='Womans_702'
+      id="Womans_"
+      data-name="Womans@"
+      width={890}
+      height={736}
+      transform="translate(218 59)"
+      xlinkHref="../Womans_702.webp"
+    />
+  </g>
+</svg>
+  )
+}
+
+export default Test
---
+++ src/app/test/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Test from './Test'
+
+const page = () => {
+  return (
+    <div className='Test'>
+        
+        <Test/>
+    </div>
+  )
+}
+
+export default page
