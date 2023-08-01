dd7a47df6611c083c4641698f1572cd0060833c3
ibrahimkarimeddin <ibrahimkarimeddin@gmail.com>
2023-08-01 15:23:18 +0300

    .

--- src/config/NavBarConfig.js
+++ src/config/NavBarConfig.js
@@ -7,7 +7,7 @@ export const NavLinks = [
     },
     {
         name:"OUR SERVICES",
-        href:"/services/FirstService"
+        href:"/services/FirstService1"
     }
     ,
     {
@@ -24,6 +24,6 @@ export const NavLinks = [
     ,
     {
         name:"MEET US",
-        href:"/meet_us"
+        href:"/meet_us1"
     }
 ]
