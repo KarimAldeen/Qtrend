93aa9bf5ad76bee7bff5b0ff5232ebf5515b928c
ibrahimkarimeddin <ibrahimkarimeddin@gmail.com>
2023-09-03 22:15:14 +0300

    ز

--- src/Components/ui/Menu.js
+++ src/Components/ui/Menu.js
@@ -4,14 +4,20 @@ export default function MenuService({handleMouseLeave, t}) {
     
     return (
         <>
-            <div className="dropdown-menu" onMouseLeave={()=>handleMouseLeave()}>
-                <Link  href={'/services?param=1'} >{t('CREATIVE')} {t('DESIGNS')}</Link>
-                <Link href={'/services?param=2'}>{t('DIGITAL')} {t('MARKETING')}</Link>
-                <Link href={'/services?param=3'}>{t('PHOTOS')} {t('&VIDEOS')}</Link>
-                <Link href={'/services?param=4'}>{t('MODELING')} {t('SERVICES')}</Link>
-                <Link href={'/FifthService'}>{t('APPS & WEB')} {t('DEVELOPMENT')}</Link>
-                <Link href={'/drag'}>{t('BLOGGERS')} {t('SERVICES')}</Link>
-            </div>
+         <div className="drop-service"  onMouseLeave={()=>handleMouseLeave()}>
+        <div className="drop-service-image">.</div>
+        <ul className="dropdown-menu">
+                <li  href={'/services?param=1'} ><p>.</p><Link  href={'/services?param=1'} >{t('CREATIVE')} {t('DESIGNS')}</Link></li>
+                <li href={'/services?param=2'}><Link href={'/services?param=2'}>{t('DIGITAL')} {t('MARKETING')}</Link></li>
+                <li href={'/services?param=3'}>                <Link href={'/services?param=3'}>{t('PHOTOS')} {t('&VIDEOS')}</Link></li>
+                <li href={'/services?param=4'}><Link href={'/services?param=4'}>{t('MODELING')} {t('SERVICES')}</Link></li>
+                <li href={'/FifthService'}>  <Link href={'/FifthService'}>{t('APPS & WEB')} {t('DEVELOPMENT')}</Link></li>
+                <li href={'/drag'}><Link href={'/drag'}>{t('BLOGGERS')} {t('SERVICES')}</Link></li>
+            </ul>
+        </div>
+                
+              
+                
 
           
         </>
