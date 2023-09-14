36bcd6e5ad2151b27b8d5f4f18a8092f29297001
MizoDaw <moazdawalibi@yahoo.com>
2023-09-14 15:27:18 +0300

    print

--- src/Components/Print/PrintNavBar.js
+++ src/Components/Print/PrintNavBar.js
@@ -5,10 +5,10 @@ const PrintNavBar = () => {
   return (
     <div className='PrintNavBar'>
         <div className='print_links'>
-            <div><Link href={'/'}>ALL PRINTS</Link></div>
-            <div className='link_with_arrow'><Link href={'/'}>PAPER PRINTS</Link></div>
-            <div className='link_with_arrow'><Link href={'/'}>PAINTING AND STICKERS</Link></div>
-            <div className='link_with_arrow'><Link href={'/'}>ADVERTISING PRODUCTS</Link></div>
+            <div><Link className='Link' href={'/'}>ALL PRINTS</Link></div>
+            <div className='link_with_arrow'><Link className='Link'  href={'/'}>PAPER PRINTS</Link></div>
+            <div className='link_with_arrow'><Link className='Link'  href={'/'}>PAINTING AND STICKERS</Link></div>
+            <div className='link_with_arrow'><Link className='Link'  href={'/'}>ADVERTISING PRODUCTS</Link></div>
 
         </div>
         <div className='print_icons'>
