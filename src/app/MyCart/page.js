2d5213c39396b18b769dde799feb5ae83dbbba5c
MizoDaw <moazdawalibi@yahoo.com>
2023-09-17 14:22:07 +0300

    mycart

--- src/app/MyCart/page.js
+++ src/app/MyCart/page.js
@@ -1,8 +1,26 @@
+"use client"
 import React from 'react'
-
+import TopHeader  from '../../Components/Print/TopHeader'
+import PrintNavBar from '../../Components/Print/PrintNavBar'
+import { MdOutlineShoppingBag } from 'react-icons/md'
 const MyCart = () => {
   return (
-    <div>MyCart</div>
+    <>
+      <TopHeader/>
+      <PrintNavBar/>
+      <div className='MyCart'>
+        <div className='MyCart_page'>
+          <span className='home_word'>Home  {'>'}</span>
+          <span className='MyCart_word'>  My Cart</span>
+          <div className='Header'>
+            <p>My Cart</p>
+            <MdOutlineShoppingBag/>
+          </div>
+        </div>
+      </div>
+
+    </>
+
   )
 }
 
