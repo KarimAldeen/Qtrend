670e6fad76ecff256c8c05ad2c03e5575f4295a4
ibrahimkarimeddin <ibrahimkarimeddin@gmail.com>
2023-09-21 16:26:26 +0300

    .

--- src/Components/Print/CategorySlider.js
+++ src/Components/Print/CategorySlider.js
@@ -6,28 +6,16 @@ import {useGetSlider} from '../../api/sliders'
 import { BaseURL, BaseURLImage } from '../../api/config';
 
 
-const data  = [
-    {
-        id:1  ,
-        img:'/Print/3.png'
-    },
-    {
-        id:2 ,
-        img:'/Print/2.png'
-    },
-    {
-        id:3  ,
-        img:'/Print/1.png'
-    }
-]
+
+
 function CategorySlider() {
  
     const {data} = useGetSlider()
     // console.log(data?.category_slider);
     const sliderRender = data?.category_slider?.map(slider =>(
         <img  style={{zIndex:"0 !important"}}
-        // src={BaseURLImage +slider.image} 
-        src="../Print/heroimage.png"
+        src={BaseURLImage +slider.image} 
+        // src="../Print/heroimage.png"
         alt={slider.id}    key={slider.id} className='image-slider-category'  />
     ))
     
