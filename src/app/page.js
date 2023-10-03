d9662510695e3cdf38e85c6b01718ab3ea8c9705
ibrahimkarimeddin <ibrahimkarimeddin@gmail.com>
2023-10-03 13:54:36 +0300

    .

--- src/app/page.js
+++ src/app/page.js
@@ -10,33 +10,28 @@ const Page = () => {
 
   const [isOffline , setIsOffline] = useState(false) 
 
-  const  handelGo = ()=>{
+   useEffect(()=>{
 
-    window.location.href = 'mazadalnokbha://';
 
-    // window.addEventListener('offline', function () {
-    //   return setIsOffline(true);
-    // });
+     window.addEventListener('offline', function () {
+       return setIsOffline(true);
+    });
 
-    // window.addEventListener('online', function () {
-    //   return setIsOffline(false);
-    // });
-  }
+     window.addEventListener('online', function () {
+      return setIsOffline(false);
+   });
+  });
 
   if(isOffline) return "You Are Offline PLease Connect in the internet "
  
   return (
     <div className='HOME_PAGE'>
-
-      <div onClick={handelGo}>
-        Go TO WhatApp 
-      </div>
-      {/* <CheckInternetComponent />
+      <CheckInternetComponent />
       <Home1 t={t}/>
       <Home2 t={t} data={data}/>
       <Home4 t={t}/>
       <Home5 t={t}/>
-      <Footer t={t} data={data} /> */}
+      <Footer t={t} data={data} />
     </div>
 
   )
